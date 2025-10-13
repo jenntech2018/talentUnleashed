b7a2fb01de3c41cea25c182e2302066361d7a791
jenntech2018 <jenntech2018@gmail.com>
2025-10-12 22:04:55 -0700

    fixed leaked secret key

--- talent_show/settings.py
+++ talent_show/settings.py
@@ -94,6 +94,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jenntech2018@gmail.com'
-EMAIL_HOST_PASSWORD = 'bxwn vhjp mkqp imcu'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'admin@TalentUnleashed.com'
 
