dd9cc7cd0f4f7eb1711755e78942c7b32d5de86a
jenntech2018 <jenntech2018@gmail.com>
2025-10-17 13:03:22 -0700

    forgot to save lol

--- landing/forms.py
+++ landing/forms.py
@@ -4,4 +4,4 @@ from .models import Contestant
 class ContestantForm(forms.ModelForm):
     class Meta:
         model = Contestant
-        fields = ['name_or_group_name', 'email', 'talent_description']
+        fields = ['name_or_group_name', 'email', 'talent_description', 'is_group', 'group_size']
