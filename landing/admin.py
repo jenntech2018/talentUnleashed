949fe7c7b6b25e14839dd1a46bdc8bdcdcbd879a
jenntech2018 <jenntech2018@gmail.com>
2025-10-20 12:20:56 -0700

    fixed register form

--- landing/admin.py
+++ landing/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Contestant
+from .models import Contestant, PartnerInquiry
 # Register your models here.
 
 
 admin.site.register(Contestant)
+admin.site.register(PartnerInquiry)
+
