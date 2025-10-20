c3c32679400cc476f6445620fb2b87f09bd671f3
jenntech2018 <jenntech2018@gmail.com>
2025-10-20 12:07:02 -0700

    fixed register form

--- talent_show/settings.py
+++ talent_show/settings.py
@@ -59,11 +59,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'talent_show.wsgi.application'
 
 # Database
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
